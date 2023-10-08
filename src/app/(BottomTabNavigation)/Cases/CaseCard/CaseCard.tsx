9505e8acb19a490a2d4d496ec8cb8b9c1468b96e
Ronnie Beggs <rbeggs@berkeley.edu>
2023-10-08 14:57:46 -0700

    [style] basic case card styling complete.

--- src/app/(BottomTabNavigation)/Cases/CaseCard/CaseCard.tsx
+++ src/app/(BottomTabNavigation)/Cases/CaseCard/CaseCard.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 
 function CaseCard() {
   return (
-    <View style={styles.caseCard}>
-      <Text>Temporary Text</Text>
-    </View>
+    <TouchableOpacity style={styles.caseCard}>
+      <View style={styles.infoContainer}>
+        <Text style={styles.titleText}>Longer Case title does it</Text>
+        <View style={styles.caseStatus}>
+          <Text style={styles.statusText}>In Progress</Text>
+        </View>
+      </View>
+      <View style={styles.imagePlaceholder} />
+    </TouchableOpacity>
   );
 }
 
--- src/app/(BottomTabNavigation)/Cases/CaseCard/styles.ts
+++ src/app/(BottomTabNavigation)/Cases/CaseCard/styles.ts
@@ -2,7 +2,42 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   caseCard: {
-    height: 100,
+    height: 137,
     width: 'auto',
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 20,
+    backgroundColor: '#fff',
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  infoContainer: {
+    flex: 0.5,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  caseStatus: {
+    height: 40,
+    width: 'auto',
+    backgroundColor: '#dcdcdc',
+    borderRadius: 10,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagePlaceholder: {
+    flex: 0.4,
+    backgroundColor: '#dcdcdc',
+    borderRadius: 10,
+  },
+  titleText: {
+    fontSize: 20,
+  },
+  statusText: {
+    fontSize: 16,
   },
 });
