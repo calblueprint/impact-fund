e4d6a0a2d05b15674441181c635d97cedf9afa72
foloy <foloy@MacBook-Pro.local>
2023-10-10 21:35:18 -0700

    current supabase update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,10 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@env";
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = (process.env.SUPABASE_URL ? process.env.SUPABASE_URL : '' )
-const supabaseAnonKey = (process.env.SUPABASE_ANON_KEY ? process.env.SUPABASE_ANON_KEY : '' )
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error('Supabase environment variables are not defined.');
+}
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default supabase;
