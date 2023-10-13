a550039b43a7f44d1997b2081964336797f26a7b
Philip Ye <97428041+philipye314@users.noreply.github.com>
2023-10-12 22:52:34 -0400

    Philipye314/imp 2 qr code scanner (#3)
    created working barcode scanner

--- src/app/(BottomTabNavigation)/Cases/QRCodeScanner/index.tsx
+++ src/app/(BottomTabNavigation)/Cases/QRCodeScanner/index.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
+import { router } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 
+enum permissions {
+  UNDETERMINED,
+  DENIED,
+  GRANTED,
+}
+
 function QRCodeScannerScreen() {
+  const [hasPermission, setHasPermission] = useState(permissions.UNDETERMINED);
+  const [scanned, setScanned] = useState<boolean>(false);
+  const [data, setData] = useState('NOTHING');
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(
+        status === 'granted' ? permissions.GRANTED : permissions.DENIED,
+      );
+    };
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = async (result: BarCodeScannerResult) => {
+    if (!scanned) {
+      setScanned(true);
+      setData(result.data);
+    }
+  };
+
+  if (hasPermission === permissions.DENIED) {
+    return <Text>CANNOT ACCESS CAMERA!</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is the QR Code Scanner screen!</Text>
+      <Text>Add a new case</Text>
+      <BarCodeScanner
+        onBarCodeScanned={handleBarCodeScanned}
+        style={[styles.scanner]}
+      />
+      <Text>Current Scanning: {data}</Text>
+      <TouchableOpacity onPress={() => router.back()} style={styles.button}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 }
