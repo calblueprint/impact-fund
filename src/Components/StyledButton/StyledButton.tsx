c79d6939137fc36e180c2653cc4bfa21c3a679a9
Suraj Rao <suraj_rao@berkeley.edu>
2023-10-17 00:05:35 -0700

    Removed organizeImports from vscode settings.json and added .eslintignore

--- src/Components/StyledButton/StyledButton.tsx
+++ src/Components/StyledButton/StyledButton.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router';
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
+
 import styles from './styles';
 
 function StyledButton({ text, file }: { text: string; file: string }) {
