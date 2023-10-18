ac025214338135ea04405f7e23dcf438ddfa2dd8
Ronnie Beggs <66931067+ronniebeggs@users.noreply.github.com>
2023-10-18 16:38:13 -0700

    [navigation] implement bottom tab navigation (#11)
    * [feat] implement bottom tab notification (copy code from previous failed PR).
    
    * [fix] update expo dependcies.
    
    * [fix] update escape-string-regexp dependency.
    
    * [feature] welcome screen (#10)
    
    * edge case and fixed nav
    
    * move styled button
    
    ---------
    
    Co-authored-by: Melissa Liu <meiliu.13@berkeley.edu>
    Co-authored-by: Stephanie Wong <stwong2@berkeley.edu>
    
    * Update Eslint and Prettier configs (#15)
    
    * Update ESLint and Prettier config to match Expo docs and most recent Typescript version
    
    * Update ESLint and Prettier config to match Expo docs and most recent Typescript version
    
    * Removed organizeImports from vscode settings.json and added .eslintignore
    
    * [auth] Login and Sign Up (#14)
    
    Basic log in and sign up authentication implemented with screens and queries
    
    * [feat] implement bottom tab notification (copy code from previous failed PR).
    
    * [fix] update expo dependcies.
    
    * [fix] update escape-string-regexp dependency.
    
    * [feat] create updates tab.
    
    * [fix] fix dependency issues.
    
    * [fix?] weird typescript errors are back.
    
    ---------
    
    Co-authored-by: me-liu <122945886+me-liu@users.noreply.github.com>
    Co-authored-by: Melissa Liu <meiliu.13@berkeley.edu>
    Co-authored-by: Stephanie Wong <stwong2@berkeley.edu>
    Co-authored-by: surajr10 <73401552+surajr10@users.noreply.github.com>
    Co-authored-by: Stephanie Wong <80373047+stephaniewong2@users.noreply.github.com>

--- src/app/(BottomTabNavigation)/Updates/index.tsx
+++ src/app/(BottomTabNavigation)/Updates/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-
 import styles from './styles';
 
 function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text>This is the /Profile/index screen!</Text>
+      <Text>This is the /Updates/index update screen!</Text>
     </View>
   );
 }
