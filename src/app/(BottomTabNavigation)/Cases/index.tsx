279659e27f7d0110b4cbf680f84dcfbbb7a9ac30
Ronnie Beggs <rbeggs@berkeley.edu>
2023-10-17 20:37:36 -0700

    [wip][feat] able to render cases for specific user after long load.

--- src/app/(BottomTabNavigation)/Cases/index.tsx
+++ src/app/(BottomTabNavigation)/Cases/index.tsx
@@ -9,19 +9,27 @@ import supabase from '../../../supabase/createClient';
 import { Case, UserUid } from '../../../types/types';
 
 function CasesScreen() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [cases, setCases] = useState<Case[]>([]);
   const [userUid, setUserUid] = useState<UserUid>(
     'f6f9223b-503a-4235-b4d9-3639d74a13d5',
   );
 
+  function fetchCasesOnLoad(id: UserUid) {
+    // fetch user Uid and compare it to default value
+    console.log('DEBUG: userUid: ', id);
+    fetchListViewCases(id).then(data => {
+      setCases(data);
+    });
+  }
+
   useEffect(() => {
     supabase.auth.getUser().then(data => {
       if (data.data.user) {
-        setUserUid(data.data.user?.id);
+        setUserUid(data.data.user.id);
+        setIsLoading(false);
+        fetchCasesOnLoad(data.data.user.id);
       }
-    });
-    fetchListViewCases(userUid).then(data => {
-      setCases(data);
     });
   }, []);
 
