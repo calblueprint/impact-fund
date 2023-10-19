51a0d853544eb66d682077a7fe0f4a9d6839e036
Stephanie Wong <stwong2@berkeley.edu>
2023-10-19 13:11:19 -0700

    fixed mount twice issue but cant unsubscribe on return

--- src/app/(BottomTabNavigation)/Cases/index.tsx
+++ src/app/(BottomTabNavigation)/Cases/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { router } from 'expo-router';
 import React, { useEffect, useState } from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
@@ -72,27 +71,6 @@ function CasesScreen() {
           </>
         )}
       </View>
-=======
-import { Link } from 'expo-router';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-
-import styles from './styles';
-import { signOutUser } from '../../../supabase/queries/auth';
-
-function CasesScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>This is the cases screen!</Text>
-      <Link href="/Cases/QRCodeScanner" asChild>
-        <TouchableOpacity style={styles.button}>
-          <Text>QR Code Scanner</Text>
-        </TouchableOpacity>
-      </Link>
-      <TouchableOpacity onPress={() => signOutUser()} style={styles.button}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
->>>>>>> 0c3f742 (persist user)
     </View>
   );
 }
