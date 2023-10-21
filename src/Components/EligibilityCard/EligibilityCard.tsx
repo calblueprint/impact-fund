e65645ab7a43a823da9a36e5f3580b2cd6763a09
Philip Ye <97428041+philipye314@users.noreply.github.com>
2023-10-21 14:43:27 -0700

    rebasing now

--- src/Components/EligibilityCard/EligibilityCard.tsx
+++ src/Components/EligibilityCard/EligibilityCard.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function EligibilityCard() {
+  const eligible = false;
   return (
     <View>
-      <Text>EligibilityCar</Text>
+      {eligible && <Text>EligibilityCar</Text>}
+      {!eligible && (
+        <Text>
+          <Text>Not Eligible</Text>
+          <Text>Opt out</Text>
+        </Text>
+      )}
     </View>
   );
 }
