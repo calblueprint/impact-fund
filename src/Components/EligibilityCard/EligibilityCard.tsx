d2c0ddeb1d77c76212ac927ec7c1797f4b5b20e6
Philip Ye <97428041+philipye314@users.noreply.github.com>
2023-10-21 14:27:34 -0700

    hello

--- src/Components/EligibilityCard/EligibilityCard.tsx
+++ src/Components/EligibilityCard/EligibilityCard.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function EligibilityCard() {
   return (
     <View>
-      <Text>EligibilityCard</Text>
+      <Text>EligibilityCar</Text>
     </View>
   );
 }
