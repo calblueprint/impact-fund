4343b58e2c683abcf24e707996d29e0ac4822fc8
Melissa Liu <meiliu1313@gmail.com>
2023-10-21 10:50:01 -0700

    screens for email and password login

---
+++ src/app/(Authentication)/Login/Email/index.tsx
@@ -0,0 +1,85 @@
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import styles from './styles';
+import { signInUser } from '../../../../supabase/queries/auth';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => signInUser(email, password)}
+      >
+        <TouchableOpacity style={styles.button} onPress={() => router.back()}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+
+      <Text>Please enter your email address.</Text>
+
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email address"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <View style={[styles.button]}>
+        <Link href="Login/Password" asChild>
+          <TouchableOpacity>
+            <Text>Next</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+}
+
+/*
+import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import styles from './styles';
+import { signInUser } from '../../../supabase/queries/auth';
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email address"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => signInUser(email, password)}
+      >
+        <Text>Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => router.back()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+*/
---
+++ src/app/(Authentication)/Login/Password/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 0,
+  },
+  button: {
+    margin: 10,
+    alignItems: 'center',
+    backgroundColor: '#339FFF',
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 310,
+    borderRadius: 5,
+    borderColor: '#C5C5C5',
+  },
+});
