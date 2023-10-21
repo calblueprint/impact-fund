8fe906f1ad68c145ae2986989cb1edf2f7d6b231
Philip Ye <97428041+philipye314@users.noreply.github.com>
2023-10-21 13:02:54 -0700

    hello

---
+++ src/app/(BottomTabNavigation)/Cases/QRCodeScanner/AddCase/styles 2.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 40,
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  image: {
+    height: '35%',
+    width: '100%',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  blurb: {
+    fontSize: 17,
+    marginTop: 10,
+    marginBottom: 50,
+  },
+
+  button: {
+    border: 'solid',
+    borderWidth: 2,
+    marginTop: 10,
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#339FFF',
+    height: '10%',
+    width: '100%',
+    borderRadius: 20,
+  },
+});
