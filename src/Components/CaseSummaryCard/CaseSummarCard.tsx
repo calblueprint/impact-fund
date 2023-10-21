429afe9a06869de482bb7e5a87cd07368efe4080
Ronnie Beggs <rbeggs@berkeley.edu>
2023-10-21 14:40:30 -0700

    [feat] build base layout.

--- src/Components/CaseSummaryCard/CaseSummarCard.tsx
+++ src/Components/CaseSummaryCard/CaseSummarCard.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import styles from './styles';
 
 export default function CaseSummaryCard() {
   return (
-    <View>
-      <Text>CaseSummaryCard</Text>
+    <View style={styles.summaryContainer}>
+      <Image
+        style={styles.imageContainer}
+        source={{
+          uri: 'https://lh3.googleusercontent.com/p/AF1QipON-jkYMcZ-fR1qI2x1LqGBhM1fHAVRBhXui5Fu=s1360-w1360-h1020',
+        }}
+      />
+      <View style={styles.headerContainer}>
+        <Text>CaseSummaryCard</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text>CaseSummaryCard</Text>
+      </View>
     </View>
   );
 }
