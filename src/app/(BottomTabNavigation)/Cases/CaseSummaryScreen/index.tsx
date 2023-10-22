174c44e5ba2ded2720d240659c70bb14d4f07bbe
Ronnie Beggs <rbeggs@berkeley.edu>
2023-10-22 14:22:06 -0700

    [feat] add navigation to CaseSummaryScreen page.

---
+++ src/app/(BottomTabNavigation)/Cases/CaseSummaryScreen/index.tsx
@@ -0,0 +1,25 @@
+import { router, useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+import { Case } from '../../../../types/types';
+
+function CasesScreen() {
+  const caseData = useLocalSearchParams() as unknown as Case;
+
+  console.log(caseData);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => router.back()} style={styles.button}>
+          <Text>Go Back</Text>
+        </TouchableOpacity>
+      </View>
+      <Text>Case Summary Screen</Text>
+    </View>
+  );
+}
+
+export default CasesScreen;
---
+++ src/app/(BottomTabNavigation)/Cases/CaseSummaryScreen/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    flex: 0.2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    width: '100%',
+    paddingRight: 30,
+  },
+  button: {
+    margin: 10,
+    alignItems: 'center',
+    backgroundColor: '#339FFF',
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  subText: {
+    fontSize: 12,
+    color: 'rgba(0, 0, 0, 0.50)',
+  },
+});
