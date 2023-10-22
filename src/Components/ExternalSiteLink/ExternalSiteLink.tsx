b36e6f5a446de677987d6a35ce8dbf541f8ebda0
Ronnie Beggs <rbeggs@berkeley.edu>
2023-10-22 16:17:21 -0700

    [feat] add routing to link within external browser.

--- src/Components/ExternalSiteLink/ExternalSiteLink.tsx
+++ src/Components/ExternalSiteLink/ExternalSiteLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Linking } from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
@@ -11,8 +11,21 @@ type ExternalLinkProps = {
 };
 
 export default function ExternalSiteLink({ text, url }: ExternalLinkProps) {
+  /**
+   * Routes user to the given url. Returns a Promise object:
+   * - If the user confirms the open dialog or the url automatically opens, the promise is resolved.
+   * - If the user cancels the open dialog or there are no registered applications for the url, the promise is rejected
+   */
+  async function openUrl() {
+    const supported = await Linking.canOpenURL(url); //To check if URL is supported or not.
+    if (supported) {
+      console.log(`routing user to: ${url}`);
+      await Linking.openURL(url); // It will open the URL on browser.
+    }
+  }
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={() => openUrl()}>
       <View style={styles.container}>
         <IonIcon name="information-circle-outline" size={25} />
         <Text style={styles.messageText}>{text}</Text>
