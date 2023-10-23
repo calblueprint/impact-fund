5f6d116b119582ad5e3fe11220624038f8ace609
Melissa Liu <meiliu1313@gmail.com>
2023-10-22 23:08:39 -0700

    continued styling

--- src/app/(Authentication)/Login/Password/styles.ts
+++ src/app/(Authentication)/Login/Password/styles.ts
@@ -4,22 +4,34 @@ import { colors } from '../../../../styles/colors';
 
 export default StyleSheet.create({
   container: {
+<<<<<<< HEAD
     backgroundColor: colors.white,
+=======
+    backgroundColor: '#fff',
+>>>>>>> 1b57398 (continued styling)
     width: '100%',
     height: '100%',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   backText: {
+<<<<<<< HEAD
     color: colors.midGrey,
     fontSize: 16,
     fontWeight: '400',
     marginLeft: 7,
+=======
+    color: '#000',
+    fontSize: 16,
+    opacity: 0.3,
+    fontWeight: '400',
+>>>>>>> 1b57398 (continued styling)
   },
   backButton: {
     marginTop: 100,
     marginBottom: 40,
     marginRight: 280,
+<<<<<<< HEAD
   },
   instructionText: {
     color: colors.black,
@@ -39,11 +51,30 @@ export default StyleSheet.create({
     fontWeight: '800',
     marginRight: 18,
     marginLeft: 18,
+=======
+    padding: 10,
+>>>>>>> 1b57398 (continued styling)
+  },
+  instructionText: {
+    color: '#000',
+    fontSize: 24,
+    fontWeight: '800',
+    marginRight: 18,
+    marginLeft: 18,
   },
   input: {
+<<<<<<< HEAD
     marginTop: 5,
     height: 40,
     borderBottomWidth: 0.75,
+=======
+    marginTop: 130,
+    marginBottom: 60,
+    height: 40,
+    margin: 18,
+    borderWidth: 1,
+    padding: 10,
+>>>>>>> 1b57398 (continued styling)
     width: 310,
     borderColor: colors.darkGrey,
   },
@@ -92,5 +123,24 @@ export default StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
     marginBottom: 20,
+  },
+  nextButton: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderRadius: 3,
+    borderColor: '#000000',
+    borderWidth: 1,
+    width: 310,
+    height: 50,
+    padding: 15,
+    opacity: 0.3,
+  },
+  errorMessage: {
+    color: '#920E1A',
+    fontSize: 16,
+    fontWeight: '400',
+    marginLeft: 20,
+    marginRight: 20,
   },
 });
