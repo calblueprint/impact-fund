8753517a7a848e4104da140f69f2e32e39df721b
Stephanie Wong <stwong2@berkeley.edu>
2023-10-30 18:59:52 -0700

    global styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  darkRed: '#C32D38',
+  midRed: '#D94545',
+  lightRed: '#FCEDED',
+  lightGreen: '#E2FFEC',
+  darkGreen: '#109365',
+  black: '#2C2C2C',
+  darkGrey: '#767676',
+  midGrey: '#C6C6C6',
+  lightGrey: '#F1F1F1',
+  white: '#FFFFFF',
+};
