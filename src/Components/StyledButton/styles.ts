5250311435413485b00a122d20b608d04909af33
Stephanie Wong <stwong2@berkeley.edu>
2023-11-01 00:21:37 -0700

    updated colors in style sheets to use global colors (minus blue back buttons which need to be updated)

--- src/Components/StyledButton/styles.ts
+++ src/Components/StyledButton/styles.ts
@@ -1,28 +1,29 @@
 import { StyleSheet } from 'react-native';
 
+import { colors } from '../../styles/colors';
+
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#999999',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   buttonText: {
     fontSize: 19,
-    color: '#000000',
+    color: colors.black,
     alignSelf: 'center',
     fontWeight: '600',
     textTransform: 'uppercase',
   },
 
   buttonContainer: {
-    backgroundColor: '#bcbcbc',
+    backgroundColor: colors.midGrey,
     width: 325,
     height: 80,
     justifyContent: 'center',
     borderRadius: 3,
-    borderColor: '#000000',
+    borderColor: colors.black,
     borderWidth: 1.5,
     marginTop: 20,
     marginBottom: 10,
