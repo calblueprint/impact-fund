464a4580b3836ff06a6bb0e383390fdcf7cd8095
Stephanie Wong <80373047+stephaniewong2@users.noreply.github.com>
2023-11-01 00:23:44 -0700

    [styles] global colors and svgs (#25)
    implemented global colors, imported svgs, updated keys so they are preceded with EXPO_PUBLIC_

--- src/app/(BottomTabNavigation)/Cases/styles.ts
+++ src/app/(BottomTabNavigation)/Cases/styles.ts
@@ -1,12 +1,14 @@
 import { StyleSheet } from 'react-native';
 
+import { colors } from '../../../styles/colors';
+
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
     width: '100%',
-    backgroundColor: '#fff',
+    backgroundColor: colors.white,
   },
   headerContainer: {
     flex: 0.2,
@@ -25,7 +27,7 @@ export default StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 100 / 2,
-    backgroundColor: '#a9a9a9',
+    backgroundColor: colors.midGrey,
     alignItems: 'center',
     justifyContent: 'center',
   },
