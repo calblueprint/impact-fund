c17d03bc4a9bb4b9468efec140a4f342075018cd
Ronnie Beggs <rbeggs@berkeley.edu>
2023-11-03 19:01:45 -0700

    [fix] resolve merge conflict.

---
+++ src/app/(BottomTabNavigation)/Cases/FormScreen/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    margin: 10,
+    alignItems: 'center',
+    backgroundColor: '#339FFF',
+    padding: 10,
+  },
+});
