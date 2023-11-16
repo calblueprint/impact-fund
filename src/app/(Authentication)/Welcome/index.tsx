211e36b2d2e51c70162481fe1afa4b04951d5006
Melissa Liu <meiliu1313@gmail.com>
2023-11-16 13:41:40 -0800

    basic welcome page

--- src/app/(Authentication)/Welcome/index.tsx
+++ src/app/(Authentication)/Welcome/index.tsx
@@ -7,14 +7,14 @@ import StyledButton from '../../../Components/StyledButton/StyledButton';
 function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text style={styles.welcomeText}>
-          Welcome to Impact Fund! Log in or create an account to get started.
-        </Text>
-      </View>
+      <Text style={styles.welcomeText}>Welcome to the Impact Fund!</Text>
       <View style={styles.buttonContainer}>
-        <StyledButton text="LOG IN" file="/Login/Email" />
-        <StyledButton text="CREATE ACCOUNT" file="/SignUp" />
+        <StyledButton text="Create an account" file="/SignUp" color="white" />
+        <StyledButton
+          text="Log in with email"
+          file="/Login/Email"
+          color="black"
+        />
       </View>
     </View>
   );
