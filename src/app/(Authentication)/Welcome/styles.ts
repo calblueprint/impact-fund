3a768de2a6bf773c386076e9b992db6806e3fd14
Melissa Liu <meiliu1313@gmail.com>
2023-11-16 13:41:40 -0800

    basic welcome page

--- src/app/(Authentication)/Welcome/styles.ts
+++ src/app/(Authentication)/Welcome/styles.ts
@@ -7,13 +7,6 @@ export default StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: colors.white,
-    justifyContent: 'flex-start',
-  },
-  textContainer: {
-    width: 325,
-    marginLeft: 30,
-    marginTop: 140,
-    marginBottom: 30,
   },
 
   welcomeText: {
@@ -28,5 +21,6 @@ export default StyleSheet.create({
 
   buttonContainer: {
     alignItems: 'center',
+    marginTop: 50,
   },
 });
