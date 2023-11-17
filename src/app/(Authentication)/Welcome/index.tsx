cbcbe3d8cd072dda5c548e056e95497b79355770
Stephanie Wong <80373047+stephaniewong2@users.noreply.github.com>
2023-11-17 08:12:18 -0800

    [styles] use pixels, add case title to case screen
    * Add route param from eligibility form to case screen
    
    * minor styling fixes to use pixels
    
    * [routing] update case context to add loading and fix routing bug for redirecting to auth  (#38)
    
    * [routing] fixed reroute to welcome screen bug and added loading to case context
    
    * add loading to dependency array
    
    * remove comment
    
    * minor styling fixes to use pixels
    
    * clean up rendering of eligibility card in case screen
    
    * [routing] fix setting load and noCasesExist
    
    * [routing] fixed reroute to welcome screen bug and added loading to case context
    
    * add loading to dependency array
    
    * remove comment
    
    * minor changes
    
    * [routing] fixed reroute to welcome screen bug and added loading to case context
    
    * minor changes
    
    * remove extra set loading
    
    * [bug] revert eligibility cards on case screen
    
    * minor styling fixes to use pixels
    
    * fix title ty suraj
    
    * update hardcoded requirements on eligibility form
    
    ---------
    
    Co-authored-by: Suraj Rao <suraj_rao@berkeley.edu>
    Co-authored-by: surajr10 <73401552+surajr10@users.noreply.github.com>

--- src/app/(Authentication)/Welcome/index.tsx
+++ src/app/(Authentication)/Welcome/index.tsx
@@ -7,10 +7,14 @@ import StyledButton from '../../../Components/StyledButton/StyledButton';
 function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome to the Impact Fund!</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.welcomeText}>
+          Welcome to Impact Fund! Log in or create an account to get started.
+        </Text>
+      </View>
       <View style={styles.buttonContainer}>
-        <StyledButton text="Create an account" file="/SignUp" color="white" />
-        <StyledButton text="Log in with email" file="/Login" color="black" />
+        <StyledButton text="LOG IN" file="/Login/Email" />
+        <StyledButton text="CREATE ACCOUNT" file="/SignUp" />
       </View>
     </View>
   );
