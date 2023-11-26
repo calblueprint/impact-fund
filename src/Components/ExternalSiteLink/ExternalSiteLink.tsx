c3212a25168a4890187d812c81aded66f693e2fa
Stephanie Wong <stwong2@berkeley.edu>
2023-11-26 10:27:47 -0800

    cleanup

--- src/Components/ExternalSiteLink/ExternalSiteLink.tsx
+++ src/Components/ExternalSiteLink/ExternalSiteLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Linking } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 import InformationIcon from '../../../assets/information-icon.svg';
@@ -12,19 +12,6 @@ type ExternalLinkProps = {
 };
 
 export default function ExternalSiteLink({ text, url }: ExternalLinkProps) {
-  // /**
-  //  * Routes user to the given url. Returns a Promise object:
-  //  * - If the user confirms the open dialog or the url automatically opens, the promise is resolved.
-  //  * - If the user cancels the open dialog or there are no registered applications for the url, the promise is rejected
-  //  */
-  // async function openUrl() {
-  //   const supported = await Linking.canOpenURL(url); //To check if URL is supported or not.
-  //   if (supported) {
-  //     console.log(`routing user to: ${url}`);
-  //     await Linking.openURL(url); // It will open the URL on browser.
-  //   }
-  // }
-
   return (
     <TouchableOpacity onPress={() => openUrl(url)}>
       <View style={styles.container}>
