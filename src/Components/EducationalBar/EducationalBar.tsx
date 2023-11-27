9860c9bd96fdf95d00bf731e4ffeeebc3f54ffe8
Stephanie Wong <stwong2@berkeley.edu>
2023-11-27 02:15:12 -0800

    fix border color

--- src/Components/EducationalBar/EducationalBar.tsx
+++ src/Components/EducationalBar/EducationalBar.tsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, View } from 'react-native';
 
 import styles from './styles';
 import RedInformationIcon from '../../../assets/red-information-icon.svg';
-import RightCarrot from '../../../assets/right-carrot.svg';
+import RightCarrot from '../../../assets/right-caret.svg';
 import { openUrl } from '../../app/(BottomTabNavigation)/AllCases/utils';
 
 export default function EducationalBar() {
