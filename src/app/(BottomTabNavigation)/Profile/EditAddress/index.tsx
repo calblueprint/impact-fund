080c7eff66f9dc20fa22aea9afd3829e5c9fec3e
foloye <41088782+foloye@users.noreply.github.com>
2023-11-30 13:15:27 -0800

    [profile] profile flow frontend hi-fis
    * env not working
    
    * package lock
    
    * wip queries
    
    * feat: settings tab
    
    * fixed merge conflicts
    
    * [cases] case summary component (#22)
    
    * [feat] build base layout.
    
    * [feat] style component with hardcoded data.
    
    * [feat] conditional render case data; fix styling.
    
    * [feat] add navigation to CaseSummaryScreen page.
    
    * [feat] pass case data into case summary screen.
    
    * [style] section off information by type.
    
    * [style] continue styling to match figma.
    
    * [wip][feat] implement base external link component.
    
    * [feat] add routing to link within external browser.
    
    * [feat] add header component with impact fund logo to all Cases related screens.
    
    * [cleanup] final cleanup before PR.
    
    * [fix] fix rendering issue on the bottom of CaseSummaryScreen.
    
    * [feat] implement date format util.
    
    * [style] restyle sub text to better reflect figma designs.
    
    * [feat] build base layout.
    
    * [feat] style component with hardcoded data.
    
    * [feat] conditional render case data; fix styling.
    
    * [feat] add navigation to CaseSummaryScreen page.
    
    * [feat] pass case data into case summary screen.
    
    * [style] section off information by type.
    
    * [style] continue styling to match figma.
    
    * [wip][feat] implement base external link component.
    
    * [feat] add routing to link within external browser.
    
    * [feat] add header component with impact fund logo to all Cases related screens.
    
    * [cleanup] final cleanup before PR.
    
    * [fix] fix rendering issue on the bottom of CaseSummaryScreen.
    
    * [feat] implement date format util.
    
    * [style] restyle sub text to better reflect figma designs.
    
    * [style] change div dimensions to percentages/aspect ratios.
    
    * [style] implement global colors.
    
    * [style] final styling changes + shadows.
    
    * [fix] prevent eslint error.
    
    * [style] replace react icons with svgs.
    
    * [refactor] restructure image fetch from supabase storage; redo typing case-related data.
    
    * [wip]
    
    * [cleanup] minor query refactoring and style fixes.
    
    * [style] setup scroll view container for CaseScreen.
    
    * [cleanup] fix styling.
     yes
    
    * profile tab rebased
    
    * env not working
    
    * idk what im doing at this point
    
    * implemented full name on profile
    
    * updated address fields
    
    * settings: edit profile queries
    
    * settings page front end done
    
    * done with this branch
    
    * profile tab: frontend done
    
    * profile tab: frontend done 2
    
    * profile: frontend bugs addressed
    
    * profile: frontend margins fixed
    
    * profile: frontend margins fixed
    
    * profile: address margins done
    
    ---------
    
    Co-authored-by: foloy <foloy@wifi-10-40-250-172.wifi.berkeley.edu>
    Co-authored-by: foloy <foloy@wifi-10-40-249-210.wifi.berkeley.edu>
    Co-authored-by: foloy <foloy@wifi-10-40-248-220.wifi.berkeley.edu>
    Co-authored-by: foloy <foloy@wifi-10-40-251-218.wifi.berkeley.edu>
    Co-authored-by: Ronnie Beggs <66931067+ronniebeggs@users.noreply.github.com>
    Co-authored-by: foloy <foloy@wifi-10-40-251-232.wifi.berkeley.edu>

--- src/app/(BottomTabNavigation)/Profile/EditAddress/index.tsx
+++ src/app/(BottomTabNavigation)/Profile/EditAddress/index.tsx
@@ -1,18 +1,31 @@
 import { router } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
+import Submit from '../../../../../assets/submit.svg';
+import AuthInput from '../../../../Components/AuthInput/AuthInput';
+import MiniAuthInput from '../../../../Components/MiniAuthInput/MiniAuthInput';
 import {
   getCurrentUserInfo,
   updateCurrUserAddress,
 } from '../../../../supabase/queries/auth';
 
 function EditNameScreen() {
-  const [streetName, setStreetName] = useState<string>();
-  const [usState, setUsState] = useState<string>();
-  const [city, setCity] = useState<string>();
-  const [zip, setZip] = useState<string>();
+  const [streetName, setStreetName] = useState<string>('');
+  const [usState, setUsState] = useState<string>('');
+  const [city, setCity] = useState<string>('');
+  const [zip, setZip] = useState<string>('');
+  const [displayStreetName, setDisplayStreetName] = useState<boolean>(true);
+  const [displayState, setDisplayState] = useState<boolean>(true);
+  const [displayCity, setDisplayCity] = useState<boolean>(true);
+  const [displayZip, setDisplayZip] = useState<boolean>(true);
+  const [streetPlaceholder, setStreetPlaceholder] =
+    useState<string>('Street Name');
+  const [statePlaceholder, setStatePlaceholder] = useState<string>('State');
+  const [cityPlaceholder, setCityPlaceholder] = useState<string>('City');
+  const [zipPlaceholder, setZipPlaceholder] = useState<string>('Zip code');
+
   useEffect(() => {
     getCurrentUserInfo().then(result => {
       setStreetName(result.streetName);
@@ -22,44 +35,93 @@ function EditNameScreen() {
     });
   }, []);
   return (
-    <View>
-      <Text>Edit Address</Text>
-      <TextInput
-        style={styles.input}
-        value={streetName}
-        onChangeText={setStreetName}
-        placeholder="Input Street Name"
-      />
-      <TextInput
-        style={styles.input}
-        value={city}
-        onChangeText={setCity}
-        placeholder="Input City Here"
-      />
-      <TextInput
-        style={styles.input}
-        value={usState}
-        onChangeText={setUsState}
-        placeholder="Input State Here"
-      />
-      <TextInput
-        style={styles.input}
-        value={zip}
-        onChangeText={setZip}
-        placeholder="Input Zip Code Here"
-      />
+    <View style={styles.container}>
       <TouchableOpacity
-        style={styles.button}
+        style={styles.backButton}
+        onPress={() => router.push('/Profile/')}
+      >
+        <Text style={styles.backText}>Back</Text>
+      </TouchableOpacity>
+      <Text style={styles.instructionText}>Edit account details</Text>
+
+      <View style={styles.inputBox}>
+        <AuthInput
+          input={streetName}
+          setInput={setStreetName}
+          defaultValue="Street Name"
+          isPassword={false}
+          displayInput={displayStreetName}
+          setDisplayInput={setDisplayStreetName}
+          keyboard="default"
+          autoCapitalization
+          placeholder={streetPlaceholder}
+          setPlaceholder={setStreetPlaceholder}
+        />
+      </View>
+      <View style={styles.inputBox}>
+        <AuthInput
+          input={city}
+          setInput={setCity}
+          defaultValue="City"
+          isPassword={false}
+          displayInput={displayCity}
+          setDisplayInput={setDisplayCity}
+          keyboard="default"
+          autoCapitalization
+          placeholder={cityPlaceholder}
+          setPlaceholder={setCityPlaceholder}
+        />
+      </View>
+      <View style={styles.stateLine}>
+        <View>
+          <MiniAuthInput
+            input={usState}
+            setInput={setUsState}
+            defaultValue="State"
+            isPassword={false}
+            displayInput={displayState}
+            setDisplayInput={setDisplayState}
+            keyboard="default"
+            autoCapitalization
+            placeholder={statePlaceholder}
+            setPlaceholder={setStatePlaceholder}
+          />
+        </View>
+        <View>
+          <MiniAuthInput
+            input={zip}
+            setInput={setZip}
+            defaultValue="Zip code"
+            isPassword={false}
+            displayInput={displayZip}
+            setDisplayInput={setDisplayZip}
+            keyboard="default"
+            autoCapitalization
+            placeholder={zipPlaceholder}
+            setPlaceholder={setZipPlaceholder}
+          />
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={
+          streetName && city && usState && zip
+            ? styles.submitButton
+            : [styles.submitButton, styles.submitButtonDisabled]
+        }
         onPress={() => {
           if (streetName && city && usState && zip) {
             updateCurrUserAddress(streetName, city, usState, zip);
             router.push('/Profile/');
           } else {
-            router.push('/Profile/');
+            //ask josh about case of invalid address
           }
         }}
       >
-        <Text>Update Address</Text>
+        <Text style={styles.submitText}>
+          Submit
+          <Submit style={styles.submitIcon} />
+        </Text>
       </TouchableOpacity>
     </View>
   );
--- src/app/(BottomTabNavigation)/Profile/EditName/styles.ts
+++ src/app/(BottomTabNavigation)/Profile/EditName/styles.ts
@@ -4,24 +4,58 @@ import { colors } from '../../../../styles/colors';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: colors.white,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    height: '100%',
+    width: '100%',
   },
-  button: {
-    margin: 10,
+  backText: {
+    color: colors.midGrey,
+    fontSize: 16,
+    fontWeight: '400',
+    marginLeft: 7,
+  },
+  backButton: {
+    marginTop: 51,
+    marginBottom: 55,
+    alignSelf: 'flex-start',
+    marginLeft: 22,
+  },
+  instructionText: {
+    color: colors.black,
+    fontSize: 24,
+    fontWeight: '800',
+    alignSelf: 'flex-start',
+    marginLeft: 29,
+    marginBottom: 27,
+  },
+  inputBox: {
+    width: '100%',
+    alignSelf: 'flex-start',
+    marginLeft: 29,
+  },
+  submitText: {
+    fontSize: 17,
+    color: colors.white,
+    fontWeight: '600',
+    marginLeft: 29,
+  },
+  submitButtonDisabled: {
+    opacity: 0.35,
+  },
+  submitIcon: {
+    marginLeft: 20,
+  },
+  submitButton: {
+    backgroundColor: colors.black,
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: colors.midGrey,
-    padding: 10,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: 310,
+    alignSelf: 'flex-start',
+    marginLeft: 29,
     borderRadius: 5,
-    borderColor: colors.midGrey,
+    marginTop: 39,
+    width: 150,
+    height: 50,
   },
 });
