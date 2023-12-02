f3e14aa42402f9f096874ad03b785f7c5f04fdf4
Ronnie Beggs <rbeggs@berkeley.edu>
2023-12-02 15:50:27 -0800

    [feat] style case cards.

--- src/Components/CaseCard/CaseCard.tsx
+++ src/Components/CaseCard/CaseCard.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 import styles from './styles';
+import ThreeDots from '../../../assets/three-dots.svg';
+import {
+  formatDate,
+  getStatusColor,
+} from '../../app/(BottomTabNavigation)/AllCases/utils';
 import { Case } from '../../types/types';
 
 function CaseCard(caseData: Case) {
+  const statusColor = getStatusColor(caseData.caseStatus);
   return (
     <TouchableOpacity
       style={styles.caseCard}
@@ -15,20 +21,35 @@ function CaseCard(caseData: Case) {
         })
       }
     >
-      <View style={styles.infoContainer}>
-        <Text style={styles.titleText} adjustsFontSizeToFit>
-          {caseData.title}
-        </Text>
-        <View style={styles.statusContainer}>
+      <View style={styles.mainContentContainer}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.titleText} adjustsFontSizeToFit>
+            {caseData.title}
+          </Text>
+          <View style={[styles.statusContainer, statusColor.background]}>
+            <Text style={[styles.statusText, statusColor.text]}>
+              {caseData.caseStatus}
+            </Text>
+          </View>
+          {/* <View style={styles.statusContainer}>
           <Text style={styles.statusText}>{caseData.caseStatus}</Text>
+        </View> */}
+        </View>
+        <Image
+          style={styles.imageContainer}
+          source={{
+            uri: caseData.imageUrl,
+          }}
+        />
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.bottomText}>
+          {formatDate(caseData.date)} • {caseData.lawFirm}
+        </Text>
+        <View style={styles.threeDots}>
+          <ThreeDots />
         </View>
       </View>
-      <Image
-        style={styles.imagePlaceholder}
-        source={{
-          uri: caseData.imageUrl,
-        }}
-      />
     </TouchableOpacity>
   );
 }
