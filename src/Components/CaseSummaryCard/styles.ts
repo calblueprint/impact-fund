66a1535b0b33e1e0b65bfeba2d539b9a362da1c6
Stephanie Wong <stwong2@berkeley.edu>
2023-12-06 22:04:37 -0800

    remove case summary card styling (in ronnies pr)

--- src/Components/CaseSummaryCard/styles.ts
+++ src/Components/CaseSummaryCard/styles.ts
@@ -11,10 +11,10 @@ export default StyleSheet.create({
     borderColor: colors.midGrey,
     borderStyle: 'solid',
     backgroundColor: colors.white,
-    shadowColor: colors.midGrey,
+    shadowColor: colors.black,
     shadowOffset: { width: 0.05, height: 0.75 },
-    shadowOpacity: 1,
-    shadowRadius: 0.75,
+    shadowOpacity: 1.25,
+    shadowRadius: 0.05,
     elevation: 1,
   },
   imageContainer: {
