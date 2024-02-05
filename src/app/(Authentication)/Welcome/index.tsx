1a2736bb8a0358ac8ea7765a542f55733fef11e5
Ronnie Beggs <rbeggs@berkeley.edu>
2024-02-04 21:22:31 -0800

    [style] fix styling nits.

--- src/app/(Authentication)/Welcome/index.tsx
+++ src/app/(Authentication)/Welcome/index.tsx
@@ -7,21 +7,25 @@ import StyledButton from '../../../Components/StyledButton/StyledButton';
 function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require('../../../../assets/inline-logo.jpeg')}
-          style={{ width: 130, height: 16.25 }}
-        />
-      </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../../../../assets/inline-logo.jpeg')}
+            style={{ width: 130, height: 16.25 }}
+          />
+        </View>
 
-      <Text style={styles.welcomeText}>Welcome to the Impact Fund!</Text>
-      <View style={styles.buttonContainer}>
-        <StyledButton text="Create an account" file="/SignUp" color="white" />
-        <StyledButton
-          text="Log in with email"
-          file="/Login/Email"
-          color="black"
-        />
+        <View style={styles.textContainer}>
+          <Text style={styles.welcomeText}>Welcome to the Impact Fund!</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <StyledButton text="Create an account" file="/SignUp" color="white" />
+          <StyledButton
+            text="Log in with email"
+            file="/Login/Email"
+            color="black"
+          />
+        </View>
       </View>
     </View>
   );
