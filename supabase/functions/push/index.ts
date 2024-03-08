01c355014ca3d1ab66d179633d33f17ecf19e26a
Stephanie Wong <stwong2@berkeley.edu>
2024-03-08 02:22:36 -0800

    move edge function logic to database function

--- supabase/functions/push/index.ts
+++ supabase/functions/push/index.ts
@@ -2,75 +2,27 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-// TODO: what's the diff btwn all of these LOL
-// import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.4';
-// import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.7.1';
-
 console.log('Hello from Functions!');
 
 Deno.serve(async req => {
-  const supabase = createClient(
-    Deno.env.get('SUPABASE_URL') ?? '',
-    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-    {
-      global: {
-        headers: { Authorization: req.headers.get('Authorization')! },
-      },
+  const { update, expoPushToken } = await req.json();
+
+  const res = await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Deno.env.get('EXPO_ACCESS_TOKEN')}`,
     },
-  );
+    body: JSON.stringify({
+      to: expoPushToken,
+      sound: 'default',
+      body: update,
+    }),
+  }).then(res => res.json());
 
-  const { update, caseUUID } = await req.json();
-
-  // TODO: this doesnt work (supabase.auth)
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser(); // rip why null
-
-  // const userId = await supabase
-  //   .from('users')
-  //   .select('userId')
-  //   .eq('userId', user.id);
-  const userId = '63bc60bf-acdf-40c5-99f5-310a84f1583b';
-
-  // const expoPushToken = await supabase
-  //   .from('users')
-  //   .select('expo_push_token')
-  //   .eq('userId', user.id);
-  const expoPushToken = 'ExponentPushToken[R5zzYfNdaOLI6-m33_7uK4]';
-
-  const { data } = await supabase
-    .from('status')
-    .select('caseId')
-    .eq('userId', userId);
-
-  const caseIds = data.map(item => item.caseId);
-
-  if (caseIds.includes(caseUUID)) {
-    const res = await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${Deno.env.get('EXPO_ACCESS_TOKEN')}`,
-      },
-      body: JSON.stringify({
-        to: expoPushToken,
-        sound: 'default',
-        body: update,
-      }),
-    }).then(res => res.json());
-
-    return new Response(JSON.stringify(res), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } else {
-    // TODO: how to handle case where we dont want a notification?
-    // cant do nothing bc errors: event loop error: TypeError: First argument to respondWith must be a Response or a promise resolving to a Response. at Object.respondWith (ext:deno_http/01_http.js:219:15) at eventLoopTick (ext:core/01_core.js:64:7)
-    const res = { message: '' };
-    return new Response(JSON.stringify(res), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+  return new Response(JSON.stringify(res), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 });
 
 /* To invoke locally:
