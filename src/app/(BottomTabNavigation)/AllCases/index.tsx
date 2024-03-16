71bad160aa9dd54e24b00b452ac0c646186d1a76
Stephanie Wong <80373047+stephaniewong2@users.noreply.github.com>
2024-03-16 16:39:38 -0700

    [notifications] setting up case specific notifications (#65)
    * progress... mostly broken :,)
    
    * fetch dummy update from supabase
    
    * progress... mostly broken :,)
    
    * fetch dummy update from supabase
    
    * working edge function?
    
    * set up edge function to work for specfiic user (me)
    
    * remove push component and old edge function
    
    * clean up
    
    * move edge function logic to database function
    
    * revert variable name
    
    * clean up
    
    * move project id
    
    * delete seed file
    
    * [refactor] reorganize notification logic/queries into new files.
    
    * [cleanup] add documentation to edge function and attempt to fix tsc issues.
    
    * [fix] resolve Deno compiler errors.
    
    * [fix] resolve eslint error by adding supabase functions to eslintignore file.
    
    ---------
    
    Co-authored-by: Ronnie Beggs <rbeggs@berkeley.edu>

--- src/app/(BottomTabNavigation)/AllCases/index.tsx
+++ src/app/(BottomTabNavigation)/AllCases/index.tsx
@@ -1,14 +1,29 @@
 import { router } from 'expo-router';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { FlatList, Text, View, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 import Camera from '../../../../assets/camera.svg';
 import CaseCard from '../../../Components/CaseCard/CaseCard';
+import { useSession } from '../../../context/AuthContext';
 import { CaseContext } from '../../../context/CaseContext';
+import {
+  registerForPushNotifications,
+  updatePushToken,
+} from '../../../supabase/pushNotifications';
+import 'react-native-url-polyfill/auto';
 
 function CasesScreen() {
   const { allCases, loading } = useContext(CaseContext);
+  const { session } = useSession();
+
+  useEffect(() => {
+    if (session?.user) {
+      registerForPushNotifications().then(async (token: string) => {
+        updatePushToken(session.user.id, token);
+      });
+    }
+  }, []);
 
   return (
     <View style={styles.container}>
