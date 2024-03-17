d340aa75e3a716a7c13a94fd532473ded34d38d6
Arfa Momin <arfamomin16@gmail.com>
2024-03-17 14:39:21 -0700

    fix: clean up ordering, remove excess containers

--- src/app/(BottomTabNavigation)/Profile/LogOut/index.tsx
+++ src/app/(BottomTabNavigation)/Profile/LogOut/index.tsx
@@ -5,49 +5,38 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 import SignOut from '../../../../../assets/sign-out.svg';
 import X from '../../../../../assets/x.svg';
-// import CasesHeader from '../../../../Components/CasesHeader/CasesHeader'; address in future PR
 import { useSession } from '../../../../context/AuthContext';
 function LogOutConfirmation() {
-  const { signOut, session } = useSession();
+  const { signOut } = useSession();
 
   return (
     <View style={styles.container}>
-      <View style={styles.line}>
-        <View style={styles.screenContainer}>
-          {/* <View style={styles.headerContainer}>
-            <CasesHeader />
-          </View> */}
+      <View style={styles.screenContainer}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.topText}>
+            Are you sure you'd like to log out?
+          </Text>
 
-          <View style={styles.contentContainer}>
-            <Text style={styles.topText}>
-              Are you sure you'd like to log out?
-            </Text>
-
-            <View style={styles.buttonContainer}>
-              <View style={styles.buttonView}>
-                <TouchableOpacity
-                  onPress={() => router.push('/Profile')}
-                  style={styles.cancelButton}
-                >
-                  <View style={styles.buttonContent}>
-                    <X />
-                    <Text style={styles.cancelText}>Cancel</Text>
-                  </View>
-                </TouchableOpacity>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              onPress={() => router.push('/Profile')}
+              style={styles.cancelButton}
+            >
+              <View style={styles.buttonContent}>
+                <X />
+                <Text style={styles.cancelText}>Cancel</Text>
               </View>
+            </TouchableOpacity>
 
-              <View style={styles.buttonView}>
-                <TouchableOpacity
-                  onPress={() => signOut()}
-                  style={styles.confirmButton}
-                >
-                  <View style={styles.buttonContent}>
-                    <SignOut />
-                    <Text style={styles.confirmText}>Confirm</Text>
-                  </View>
-                </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => signOut()}
+              style={styles.confirmButton}
+            >
+              <View style={styles.buttonContent}>
+                <SignOut />
+                <Text style={styles.confirmText}>Confirm</Text>
               </View>
-            </View>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
