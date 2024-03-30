51f8bb76ccfc99889000401c2f842d167bc5a7c4
Stephanie Wong <80373047+stephaniewong2@users.noreply.github.com>
2024-03-30 15:38:01 -0700

    [updates] set up case updates (#70)
    * start
    
    * basic updates set up for cases
    
    * start
    
    * basic updates set up for cases
    
    * attempt to start styling
    
    * styled view updates screen, progress styling individual update screen
    
    * style update view
    
    * address changes
    
    * [cleanup] cleanup style.
    
    * [cleanup] fix updateView styling and remove unnecessary params.
    
    ---------
    
    Co-authored-by: Ronnie Beggs <rbeggs@berkeley.edu>

---
+++ src/Components/UpdateItem/UpdateItem.tsx
@@ -0,0 +1,28 @@
+import { router } from 'expo-router';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+import { formatDate } from '../../app/(BottomTabNavigation)/AllCases/utils';
+import { Update } from '../../types/types';
+
+export default function UpdateItem(updateData: Update) {
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        router.push({
+          pathname: `/AllCases/Updates/UpdateView/${updateData.updateUid}`,
+        })
+      }
+    >
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.titleText}>{updateData.title}</Text>
+          <Text style={styles.bottomText}>
+            {formatDate(updateData.date)} • {updateData.lawFirm}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
