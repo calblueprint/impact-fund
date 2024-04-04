3876b39ac0a4ad630bafa46f65680ccf79368158
Arfa Momin <arfamomin16@gmail.com>
2024-04-03 18:19:06 -0700

    feat: add functionality to opt out link button

--- src/app/(BottomTabNavigation)/AllCases/OptOut/[caseUid].tsx
+++ src/app/(BottomTabNavigation)/AllCases/OptOut/[caseUid].tsx
@@ -1,32 +1,26 @@
-import { Image } from 'expo-image';
 import { router, useLocalSearchParams } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
-import Check from '../../../../../assets/check-circle.svg';
-import Error from '../../../../../assets/exclamation.svg';
-import Ex from '../../../../../assets/x.svg';
-import PressableRequirement from '../../../../Components/PressableRequirement/PressableRequirement';
 import {
   updateCaseStatus,
   getCaseById,
 } from '../../../../supabase/queries/cases';
-import { getRequirementsByCaseUid } from '../../../../supabase/queries/eligibility';
-import {
-  Case,
-  CaseUid,
-  Eligibility,
-  EligibilityRequirement,
-} from '../../../../types/types';
+import { Case, CaseUid } from '../../../../types/types';
+import { openUrl } from '../utils';
+
+const ensureURLFormat = (url: string | null | undefined) => {
+  if (!url) return null;
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  }
+  return `https://${url}`;
+};
 
 export default function OptOutScreen() {
   const { caseUid } = useLocalSearchParams<{ caseUid: CaseUid }>();
   const [caseData, setCaseData] = useState<Case>();
-  const [eligibilityRequirements, setEligibilityRequirements] = useState<
-    EligibilityRequirement[]
-  >([]);
-  const [checkCount, setCheckCount] = useState(0);
 
   async function fetchCaseData() {
     if (caseUid) {
@@ -35,26 +29,30 @@ export default function OptOutScreen() {
     }
   }
 
-  async function updateEligibility(status: Eligibility) {
-    if (caseUid !== undefined) {
-      await updateCaseStatus(caseUid, status);
-      router.back();
-    }
-  }
-
   useEffect(() => {
     fetchCaseData();
   }, []);
 
+  const optOutLink = caseData?.optOutLink
+    ? ensureURLFormat(caseData.optOutLink)
+    : null;
+  const onPressHandler = optOutLink ? () => openUrl(optOutLink) : undefined;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>Opt Out of Case</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.bodyText}>Take Me to Link to Opt Out</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.bodyText}>I've Already Opted Out</Text>
-      </TouchableOpacity>
+      {caseData === undefined ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text style={styles.titleText}>Opt Out of Case</Text>
+          <TouchableOpacity style={styles.button} onPress={onPressHandler}>
+            <Text style={styles.bodyText}>Take Me to Link to Opt Out</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.bodyText}>I've Already Opted Out</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
