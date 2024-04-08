912a38fb937a643d85f7f0a40f9022a059249ecf
Arfa Momin <arfamomin16@gmail.com>
2024-04-08 01:15:09 -0700

    feat: more button functionality

---
+++ src/app/(BottomTabNavigation)/AllCases/OptOut/ConfirmOptOut/[caseUid].tsx
@@ -0,0 +1,69 @@
+import { router } from 'expo-router';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import styles from './styles';
+import CircleCheckWhite from '../../../../../../assets/circle-check-white.svg';
+// import Warning from '../../../../../../assets/open-warning.svg';
+import X from '../../../../../../assets/x.svg';
+import CasesHeader from '../../../../../Components/CasesHeader/CasesHeader';
+import { useSession } from '../../../../../context/AuthContext';
+import {
+  updateCaseStatus,
+  getCaseById,
+} from '../../../../../supabase/queries/cases';
+function DeleteAccountScreen() {
+  const { signOut, deleteCurrentUser, session } = useSession();
+
+  const deleteAccount = () => {
+    if (session?.user.id) {
+      deleteCurrentUser(session.user.id);
+    }
+    signOut();
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.screenContainer}>
+        <View style={styles.headerContainer}>
+          <CasesHeader />
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={styles.topText}>Indicate that you've opted out?</Text>
+          <Text style={styles.blurb}>
+            Deleting your account will also permanently delete any data
+            assoicated with it. This action cannot be undone.
+          </Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonView}>
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={styles.cancelButton}
+            >
+              <View style={styles.buttonContent}>
+                <X />
+                <Text style={styles.cancelText}>Cancel</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.buttonView}>
+            <TouchableOpacity
+              onPress={deleteAccount}
+              style={styles.confirmButton}
+            >
+              <View style={styles.buttonContent}>
+                <CircleCheckWhite />
+                <Text style={styles.confirmText}>Confirm</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+export default DeleteAccountScreen;
