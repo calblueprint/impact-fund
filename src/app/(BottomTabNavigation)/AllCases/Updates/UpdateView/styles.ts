86efc1b8fd9c7e0fbb26870fed2bfc67467e926c
Ronnie Beggs <rbeggs@berkeley.edu>
2024-04-08 01:34:45 -0700

    [style] style the detailed update view screen.

--- src/app/(BottomTabNavigation)/AllCases/Updates/UpdateView/styles.ts
+++ src/app/(BottomTabNavigation)/AllCases/Updates/UpdateView/styles.ts
@@ -10,20 +10,63 @@ export default StyleSheet.create({
     alignItems: 'center',
   },
   outerScroll: {
-    width: '84%',
-    height: '84%',
+    width: '93%',
+    paddingVertical: 20,
   },
   innerScroll: {
-    paddingVertical: 20,
+    paddingVertical: 32,
+    paddingHorizontal: 23,
     rowGap: 20,
+    backgroundColor: colors.white,
+
+    borderWidth: 1,
+    borderRadius: 15,
+    borderStyle: 'solid',
+    borderColor: colors.midGrey,
+
+    shadowColor: colors.midGrey,
+    shadowOffset: { width: 0.05, height: 0.75 },
+    shadowOpacity: 1.25,
+    shadowRadius: 0.05,
+    elevation: 1,
   },
-  title: {
-    color: colors.midnightBlack,
-    fontSize: 30,
-    fontStyle: 'normal',
-    fontWeight: '800',
+  titleContainer: {
+    flexDirection: 'row',
+    columnGap: 13,
   },
   inLineSubInfo: {
     flexDirection: 'row',
+    height: 30,
+
+    borderBottomWidth: 0.5,
+    borderBottomColor: colors.midGrey,
+  },
+  titleText: {
+    fontSize: 20,
+    lineHeight: 24,
+    fontWeight: '600',
+    fontStyle: 'normal',
+    color: colors.black,
+    flex: 1,
+  },
+  bodyText: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontStyle: 'normal',
+    fontWeight: '200',
+    color: colors.black,
+  },
+  subText: {
+    fontSize: 12,
+    fontStyle: 'normal',
+    lineHeight: 21,
+  },
+  lawFirmText: {
+    color: colors.darkRed,
+    fontWeight: '700',
+  },
+  dateText: {
+    color: colors.darkGrey,
+    fontWeight: '300',
   },
 });
