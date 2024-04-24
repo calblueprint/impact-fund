1c81c3269c91992f4d61f2a5fc33a8cb14b85a54
Ronnie Beggs <rbeggs@berkeley.edu>
2024-04-23 21:02:31 -0700

    [cleanup] remove comments.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,17 +5,6 @@ import { colors } from './colors';
 export default StyleSheet.create({
   shadowBorder: {
     backgroundColor: colors.white,
-    // border: 'solid',
-    // borderColor: colors.midGrey,
-    // borderWidth: 1,
-    // borderRadius: 5,
-
-    // shadowColor: colors.midGrey,
-    // shadowOffset: { width: 0.05, height: 0.75 },
-    // shadowOpacity: 1.25,
-    // shadowRadius: 0.05,
-    // elevation: 1,
-
     borderColor: colors.midGrey,
     borderWidth: 1,
     borderStyle: 'solid',
