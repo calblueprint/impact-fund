7c0a3bdc9d615f736fbf1a65d244495e7d3b0964
Ronnie Beggs <rbeggs@berkeley.edu>
2024-05-13 17:29:16 -0700

    [feat] create a global fonts file according to style guide on figma.

---
+++ src/styles/fonts.ts
@@ -0,0 +1,67 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from './colors';
+
+const baseFonts = StyleSheet.create({
+  tabHeading: {
+    fontSize: 32,
+    fontWeight: '800',
+    fontStyle: 'normal',
+    color: colors.black,
+  },
+  headline: {
+    fontSize: 22,
+    fontWeight: '700',
+    fontStyle: 'normal',
+    color: colors.black,
+  },
+  condensedHeadline: {
+    fontSize: 20,
+    fontWeight: '600',
+    fontStyle: 'normal',
+    color: colors.midnightBlack,
+  },
+  body: {
+    fontSize: 16,
+    fontWeight: '300',
+    fontStyle: 'normal',
+    color: colors.black,
+  },
+  button: {
+    fontSize: 16,
+    fontWeight: '600',
+    fontStyle: 'normal',
+  },
+  verySmall: {
+    fontSize: 12,
+    fontWeight: '300',
+    fontStyle: 'normal',
+    color: colors.black,
+  },
+  errorMessage: {
+    fontSize: 14,
+    fontWeight: '300',
+    fontStyle: 'normal',
+    color: colors.midRed,
+  },
+});
+
+export const fonts = StyleSheet.create({
+  ...baseFonts,
+  greyBody: {
+    ...baseFonts.body,
+    color: colors.darkGrey,
+  },
+  greyVerySmall: {
+    ...baseFonts.verySmall,
+    color: colors.darkGrey,
+  },
+  blackButton: {
+    ...baseFonts.button,
+    color: colors.black,
+  },
+  whiteButton: {
+    ...baseFonts.button,
+    color: colors.white,
+  },
+});
