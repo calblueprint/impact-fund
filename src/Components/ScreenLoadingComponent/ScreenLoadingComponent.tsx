5e1f81a52e046f2953fe4c6bcd1069f3fa9392d4
Ronnie Beggs <rbeggs@berkeley.edu>
2024-06-16 23:23:23 -0700

    [wip][feat] configure timeout behavior for poor network connection settings.

--- src/Components/ScreenLoadingComponent/ScreenLoadingComponent.tsx
+++ src/Components/ScreenLoadingComponent/ScreenLoadingComponent.tsx
@@ -1,12 +1,31 @@
-import { View } from 'react-native';
+import { router } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 
 import { colors } from '../../styles/colors';
 
 export default function ScreenLoadingComponent() {
+  const [timeoutExists, setTimeoutExists] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log('timeout useeffect begins');
+    setTimeout(() => {
+      setTimeoutExists(true);
+    }, 5000);
+  }, []);
+
   return (
     <View style={{ marginTop: 20 }}>
       <ActivityIndicator size="small" color={colors.midRed} />
+      {timeoutExists && (
+        <View>
+          <Text>This is taking a while...</Text>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Text>Press Here to Navigate back to the home screen</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
